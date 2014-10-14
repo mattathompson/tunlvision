064043faa6fb039531ee8a8c400f33b1b76e4a23
Matthew Thompson <matt@mattathompson.org>
2014-10-14 01:45:05 -0400

    Adding basic static pages like about help contact

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   root 'static_pages#dashboard'
   match '/project/info', to: 'projects#info', via: 'get'
   match '/project/inspiration', to: 'projects#inspiration', via: 'get'
+  match '/about', to: 'static_pages#about', via: 'get'
+  match '/contact', to: 'static_pages#contact', via: 'get'
+  match '/help', to: 'static_pages#help', via: 'get'
 
 
   resources :projects, except: [:index] do
@@ -10,6 +13,7 @@ Rails.application.routes.draw do
       get 'deliverables_plan'
     end
     resources :collaborators
+    resources :notes
     resources :deliverables
     resources :image_attachments
     resources :video_attachments
