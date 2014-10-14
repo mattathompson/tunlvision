1890c7c283af493aadc2360666dc3b198c3c8d5a
Matthew Thompson <matt@mattathompson.org>
2014-10-14 03:40:23 -0400

    Smooth scrolling with ancor links and some Jquery.

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,4 +1,5 @@
 class NotesController < ApplicationController
+  before_action :find_project
 
   def index
 
@@ -11,19 +12,43 @@ class NotesController < ApplicationController
 
 
   def new
+    @note = @project.notes.new
+  end
+
+
+  def edit
 
   end
 
 
   def create
+    @note = @project.notes.new create_params
+    if @note.save!
+      redirect_to @project, :flash => {:success => "You created a note for #{@project.title}!"}
+    else
+      redirect_to :back, :flash => {:failure => "Sorry Something went wrong :("}
+    end
+  end
+
+
+  def update
 
   end
 
-  def 
 
+  def destroy
 
+  end
 
+private
 
+  def create_params
+    params.require(:note).permit(:title, :content)
+  end
+
+  def find_project
+    @project = current_user.projects.find params[:project_id]
+  end
 
 
 end
